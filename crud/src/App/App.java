48696d0090efcaa83b467d4b80e00ef2d11e3ade
Pedro Valente <pedroarthurvalente@gmail.com>
2025-11-06 11:35:29 -0300

    Classe App alterada

--- crud/src/App/App.java
+++ crud/src/App/App.java
@@ -2,5 +2,6 @@ package App;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
